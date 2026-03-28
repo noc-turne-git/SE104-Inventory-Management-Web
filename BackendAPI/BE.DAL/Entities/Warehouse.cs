f0d81c496c2bf509e7110e667d3b9dfcf5fb033f
huongdtq06-cloud <huong.dtq06@gmail.com>
2026-03-28 11:02:59 +0700

    fix(auth)

--- BackendAPI/BE.DAL/Entities/Warehouse.cs
+++ BackendAPI/BE.DAL/Entities/Warehouse.cs
@@ -1,15 +1,18 @@
+
 namespace BackendAPI.BE.DAL.Entities;
 
-public class Organization
+public class Warehouse
 {
-    public int Id { get; set; }
+    public int WarehouseId { get; set; }
     public string Name { get; set; } = null!;
+    public string Location { get; set; } = null!;
     public int CreatorId { get; set; } 
     
     // Navigation properties
     public User Creator { get; set; } = null!;
-    public ICollection<OrganizationMember> OrganizationMembers { get; set; } = null!;
+    public ICollection<WarehouseStaff> WarehouseStaffs { get; set; } = null!;
     public ICollection<Product> Products { get; set; } = null!;
     public ICollection<Supplier> Suppliers { get; set; } = null!;
     public ICollection<Shift> Shifts { get; set; } = null!;
+    public ICollection<InfractionTicket> InfractionTickets { get; set; } = null!;
 }
