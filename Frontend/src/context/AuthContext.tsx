e921541d45988880ed3c840f907f79e753adbb58
zpzvd <littlesophia.0506@gmail.com>
2026-04-28 04:26:19 +0700

    refactor: update Sidebar and signout flow with warehouse context

--- Frontend/src/context/AuthContext.tsx
+++ Frontend/src/context/AuthContext.tsx
@@ -1,47 +1,52 @@
 import React, { createContext, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {type User} from '../types/user'
+import { useWarehouseContext } from "./WarehouseContext";
 
 interface AuthContextType {
   user: User | null;
-  setUser: (userData: User) => void;
-  login: (userData: User) => void;
-  logout: () => void;
+  // setUser: (userData: User) => void;
+  signin: (userData: User) => void;
+  signout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
+const { clearWarehouse } = useWarehouseContext();
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(()=> {
-    const savedUser = localStorage.getItem('user');
-    return savedUser ? JSON.parse(savedUser) : null;
+    try {
+      const savedUser = localStorage.getItem('user');
+      return savedUser ? JSON.parse(savedUser) : null;
+    } catch {
+      return null;
+    }
   });
 
   const navigate = useNavigate();
 
-  
-
-  const login = (userData: User) => {
+  const signin = (userData: User) => {
     // setUser(userData);
     // console.log(user?.role);
     // console.log(user?.email);
     // navigate('/warehouse', {replace: false});
 
+    localStorage.setItem('user', JSON.stringify(userData));
     setUser(userData);
     console.log(userData); // Chắc chắn sẽ ra 'staff'
     console.log(userData.email); // Chắc chắn sẽ ra 'bob@example.com'
     navigate('/warehouse');
-    localStorage.setItem('user', JSON.stringify(userData));
   };
 
-  const logout = () => {
+  const signout = () => {
     setUser(null);
-    navigate('/home', {replace: true});
+    clearWarehouse();
     localStorage.removeItem('user');
+    navigate('/home', {replace: true});
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, setUser }}>
+    <AuthContext.Provider value={{ user, signin, signout }}>
       {children}
     </AuthContext.Provider>
   );
