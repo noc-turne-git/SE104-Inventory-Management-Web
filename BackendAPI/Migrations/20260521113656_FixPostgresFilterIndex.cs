022de303015739bc1d986152ae223d23a5c32a60
huongdtq06-cloud <huong.dtq06@gmail.com>
2026-05-21 18:37:55 +0700

    fix(DB_OTP): fix syntax và tạo migrations

---
+++ BackendAPI/Migrations/20260521113656_FixPostgresFilterIndex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPostgresFilterIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "OTPs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Expiration",
+                value: new DateTime(2026, 1, 12, 0, 15, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OTPs_Email",
+                table: "OTPs",
+                column: "Email",
+                unique: true,
+                filter: "\"IsUsed\" = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OTPs_Email",
+                table: "OTPs");
+
+            migrationBuilder.UpdateData(
+                table: "OTPs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Expiration",
+                value: new DateTime(2026, 1, 12, 0, 10, 0, 0, DateTimeKind.Utc));
+        }
+    }
+}
