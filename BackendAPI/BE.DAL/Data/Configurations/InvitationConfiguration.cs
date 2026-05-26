591b2bc38cd872bd930b1ed200125b8a2dfad876
Qhuongg <huong.dtq06@gmail.com>
2026-05-26 09:58:48 +0700

    Revert "new seed data" (#43)

--- BackendAPI/BE.DAL/Data/Configurations/InvitationConfiguration.cs
+++ BackendAPI/BE.DAL/Data/Configurations/InvitationConfiguration.cs
@@ -1,5 +1,7 @@
 namespace BackendAPI.BE.DAL.Data.Configurations;
 using BackendAPI.BE.DAL.Entities;
+using BackendAPI.BE.DAL.Constants;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 public class InvitationConfiguration : IEntityTypeConfiguration<Invitation>
@@ -28,6 +30,38 @@ public class InvitationConfiguration : IEntityTypeConfiguration<Invitation>
             .HasForeignKey(i => i.InviterUserId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        builder.HasData(SeedData.Invitations);
+        // Seed data
+        builder.HasData(
+            new Invitation
+            {
+                InvitationId = 1,
+                WarehouseId = 1,
+                InvitedUserId = 3,
+                InviterUserId = 1,
+                Role = RoleCode.STAFF,
+                CreatedAt = new DateTime(2026, 01, 01, 0, 0, 0, DateTimeKind.Utc),
+                Status = StatusCode.PENDING
+            },
+            new Invitation
+            {
+                InvitationId = 2,
+                WarehouseId = 2,
+                InvitedUserId = 1,
+                InviterUserId = 2,
+                Role = RoleCode.MANAGER,
+                CreatedAt = new DateTime(2026, 01, 02, 0, 0, 0, DateTimeKind.Utc),
+                Status = StatusCode.APPROVED
+            },
+            new Invitation
+            {
+                InvitationId = 3,
+                WarehouseId = 2,
+                InvitedUserId = 3,
+                InviterUserId = 2,
+                Role = RoleCode.STAFF,
+                CreatedAt = new DateTime(2026, 01, 03, 0, 0, 0, DateTimeKind.Utc),
+                Status = StatusCode.REJECTED
+            }
+        );
     }
 }
